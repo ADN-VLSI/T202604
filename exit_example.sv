fa73f077169cd5e23503af84070c44f026ff7ce5
Tusha Falia <59922994+TushaFalia@users.noreply.github.com>
2026-04-19 11:38:01 +0600

    Delete exit_example.sv

--- exit_example.sv
+++
@@ -1,7 +0,0 @@
-module exit_example;
-  initial begin
-    #5ns $display("Exiting simulator abruptly...");
-    $exit; // Simulator terminates immediately, potentially without full cleanup
-    $display("This line may or may not be printed, depending on the tool.");
-  end
-endmodule
