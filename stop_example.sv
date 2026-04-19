4d2ca806d56fd85577f605436efd6d2504e7c6b1
Tusha Falia <59922994+TushaFalia@users.noreply.github.com>
2026-04-19 11:39:35 +0600

    Delete stop_example.sv

--- stop_example.sv
+++
@@ -1,7 +0,0 @@
-module stop_example;
-  initial begin
-    #20ns $display("Pausing simulation...");
-    $stop; // Simulation pauses at time 20ns
-    #10ns $display("Simulation Resumed."); // Only executed if you manually resume
-  end
-endmodule
