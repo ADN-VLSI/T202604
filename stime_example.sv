e0a5a1a8307bd71b77d79ec6d0c83fd5473528a0
Tusha Falia <59922994+TushaFalia@users.noreply.github.com>
2026-04-19 11:39:25 +0600

    Delete stime_example.sv

--- stime_example.sv
+++
@@ -1,7 +0,0 @@
-module stime_example;
-  initial begin
-    #100000ns;
-    $display("32-bit Time: %0d ns", $stime);
-    // Output: "32-bit Time: 100000 ns" (May truncate or wrap in very long simulations)
-  end
-endmodule
