55126a635760a5110ed294d36c6e6e19c3810853
Tusha Falia <59922994+TushaFalia@users.noreply.github.com>
2026-04-19 11:37:36 +0600

    Delete dumpfile_example.sv

--- dumpfile_example.sv
+++
@@ -1,7 +0,0 @@
-module dumpfile_example;
-  initial begin
-    $dumpfile("signals.vcd"); // Creates a VCD file named 'signals.vcd' in the simulation directory
-    $dumpvars; // Enable dumping of all variables in the design (for simplicity in this example)
-    #100 $finish;
-  end
-endmodule
