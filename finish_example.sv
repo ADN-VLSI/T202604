c9ac94e0ae5369eb0b95a223ab4cb9bb5f7dee5a
Tusha Falia <59922994+TushaFalia@users.noreply.github.com>
2026-04-19 11:38:14 +0600

    Delete finish_example.sv

--- finish_example.sv
+++
@@ -1,7 +0,0 @@
-module finish_example;
-  initial begin
-    #10ns $display("Simulation about to finish.");
-    $finish; // Ends simulation execution here
-    $display("This line will NOT be printed."); // Simulation terminated above
-  end
-endmodule
