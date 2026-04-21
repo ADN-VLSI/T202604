cebb8f5afca9ae9ba72f414cef3fc5b78980e0d1
Foez Ahmed <foez.official@gmail.com>
2026-04-21 11:01:45 +0600

    Enhance potato_tb: Add random loading and comprehensive checks for all operations

--- practice/foez/session_20260421_a/potato_tb.sv
+++ practice/foez/session_20260421_a/potato_tb.sv
@@ -13,21 +13,55 @@ module potato_tb;
   logic [3:0] tb_c;
 
   potato dut (
-    .a(tb_a),
-    .b(tb_b),
-    .op(tb_op),
-    .c(tb_c)
+      .a (tb_a),
+      .b (tb_b),
+      .op(tb_op),
+      .c (tb_c)
   );
 
-  initial begin
-   
-    // Test case 1: Addition
-    tb_a  = 3;
-    tb_b  = 5;
-    tb_op = 2; // Addition
-    #10;
+  function automatic void load_random();
+    tb_a  <= $urandom;
+    tb_b  <= $urandom;
+    tb_op <= $urandom;
+  endfunction
 
-    // TODO : Add more test cases for other operations (Bitwise OR, Bitwise AND, Bitwise XOR)
+  function automatic void check();
+    case (tb_op)
+
+      2'b00:
+      if (tb_c !== (tb_a | tb_b)) begin
+        $fatal(1, "Error: OR  operation failed for a=%b, b=%b, c=%b", tb_a, tb_b, tb_c);
+      end
+
+      2'b01:
+      if (tb_c !== (tb_a & tb_b)) begin
+        $fatal(1, "Error: AND operation failed for a=%b, b=%b, c=%b", tb_a, tb_b, tb_c);
+      end
+
+      2'b10:
+      if (tb_c !== (4'b1111 & (tb_a + tb_b))) begin
+        $fatal(1, "Error: ADD operation failed for a=%b, b=%b, c=%b", tb_a, tb_b, tb_c);
+      end
+
+      2'b11:
+      if (tb_c !== (tb_a ^ tb_b)) begin
+        $fatal(1, "Error: XOR operation failed for a=%b, b=%b, c=%b", tb_a, tb_b, tb_c);
+      end
+
+      default: $fatal(1, "Error: Invalid opcode %b", tb_op);
+
+    endcase
+  endfunction
+
+  initial begin
+
+    repeat (100) begin
+      load_random();
+      #10;
+      check();
+    end
+
+    $display("All tests passed.");
 
     $finish;
 
