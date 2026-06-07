dec21beabc685e9dcefcc5bd5c0c2822ae0a013a
TushaFalia <tushafalia@gmail.com>
2026-06-07 13:02:24 +0600

    1st edition

--- practice/tusha/session_20260607/clk_freq_div.sv
+++ practice/tusha/session_20260607/clk_freq_div.sv
@@ -8,5 +8,38 @@ module clk_freq_div #(
 );
 
   // YOUR CODE HERE
+logic [DIV_WIDTH-1:0] counter_q;
+logic [DIV_WIDTH-1:0] counter_n;
+logic toggle_en;
+
+always_comb toggle_en = (counter_q == '0);
+
+always_comb begin
+    if (div_i == '0) begin
+      counter_n = '0;
+    end else begin
+      counter_n = counter_q + 1;
+      if (counter_n >= div_i) begin
+        counter_n = '0;
+      end
+    end
+  end
+
+  always @(clk_i or negedge arst_ni) begin
+    if (~arst_ni) begin
+      counter_q <= '0;
+    end else begin
+      counter_q <= counter_n;
+    end
+  end
+    always @(clk_i or negedge arst_ni) begin
+        if (~arst_ni) begin
+        en_o <= '0;
+        end else begin
+        if (toggle_en) begin
+            en_o <= ~en_o;
+        end
+        end
+    end
 
 endmodule
