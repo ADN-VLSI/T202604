f2145dd275b80b8880e60bee08eda617b5710501
Foez Ahmed <foez.official@gmail.com>
2026-06-07 12:13:51 +0600

    new file:   practice/ahanaf/session_20260607/clk_freq_div.sv new file:   practice/dip/session_20260607/clk_freq_div.sv new file:   practice/ibrahim/session_20260607/clk_freq_div.sv new file:   practice/jannat/session_20260607/clk_freq_div.sv new file:   practice/sajjad/session_20260607/clk_freq_div.sv new file:   practice/tusha/session_20260607/clk_freq_div.sv

---
+++ practice/ibrahim/session_20260607/clk_freq_div.sv
@@ -0,0 +1,12 @@
+module clk_freq_div #(
+    parameter DIV_WIDTH = 20
+) (
+    input  logic                 arst_ni,
+    input  logic                 clk_i,
+    input  logic [DIV_WIDTH-1:0] div_i,
+    output logic                 en_o
+);
+
+  // YOUR CODE HERE
+
+endmodule
