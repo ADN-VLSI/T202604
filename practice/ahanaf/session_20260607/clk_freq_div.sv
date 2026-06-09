8c115b6ee1632ab55d42f5c89ee7c84d0e12dea1
Ahanaf6789 <ahanafshahariarchamak@gmail.com>
2026-06-09 10:26:05 +0600

    Fix issue #40: Implement clock frequency divider (#48)

--- practice/ahanaf/session_20260607/clk_freq_div.sv
+++ practice/ahanaf/session_20260607/clk_freq_div.sv
@@ -9,4 +9,77 @@ module clk_freq_div #(
 
   // YOUR CODE HERE
 
+
+  logic [DIV_WIDTH-1:0] count_q;
+
+  always_ff @(posedge clk_i or negedge arst_ni) begin
+
+    if (!arst_ni) begin
+      count_q <= '0;
+      en_o    <= 1'b0;
+    end 
+    
+    else begin
+
+      if (div_i <= 1) begin
+        count_q <= '0;
+        en_o    <= 1'b1;
+      end 
+      
+      else if (count_q == '0) begin
+        count_q <= div_i - 1'b1;
+        en_o    <= 1'b1;
+      end 
+      
+      else begin
+        count_q <= count_q - 1'b1;
+        en_o    <= 1'b0;
+      end
+
+    end
+
+
+  end
+
+
+
+
+  /*
+                logic [DIV_WIDTH-1:0] count_q;
+
+                always_ff @(negedge clk_i) begin
+
+                        if (!arst_ni) begin
+                            count_q <= '0;
+                            en_o    <= 1'b0;
+                        end
+
+                        else 
+
+                        begin
+                            if (div_i <= 1) 
+                            begin
+                                count_q <= '0;
+                                en_o    <= 1'b1;
+                            end 
+                                
+                            else if (count_q == '0) 
+                            begin
+                                count_q <= div_i - 1;
+                                en_o    <= 1'b1;
+                            end 
+                                
+                            else 
+                            begin
+                                count_q <= count_q - 1;
+                                en_o    <= 1'b0;
+                            end
+
+                        end
+                end
+
+  */
+
+
+
 endmodule
